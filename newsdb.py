1f649eae47aa71e958fb65e4e7d420373953f57a
Dana <summer1984@users.noreply.github.com>
2018-10-27 16:46:27 -0700

    Add get errors method

--- newsdb.py
+++ newsdb.py
@@ -1,47 +1,66 @@
 #!/usr/bin/python3
 
-import psycopg2, bleach
+import psycopg2
+import bleach
 
-# Requires hits view count for time an article slug appears in the server log
+# TODO refactor - connect once/create connection function
 
-"""
-Test connection to db
-def test_connection():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("select * from articles limit 5")
-"""
+# def test_connection():
+#     '''Tests connection to the database'''
+#
+#     db = psycopg2.connect("dbname=news")
+#     c = db.cursor()
+#     c.execute("select * from articles limit 5")
+
 
 def get_top_three_articles():
-  """Returns the most popular three articles of all time, sorted by the most popular at the top."""
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  # Execute a query for the top three articles using 'popular' view of log table
-  query = ("select * from popular limit 3")
-  c.execute(query)
-  # Fetch the results of the query then close the db connection
-  articles = c.fetchall()
-  db.close()
-  # Print the article title and no. of hits
-  for article in articles:
-      title_format = article[0]
-      title = title_format.title()
-      hits = article[1]
-      print ('{} – {} views'.format(title, hits))
+    '''Prints the three most popular articles of all time'''
 
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    # query for the top three articles using 'popular' view
+    query = ("select * from popular limit 3")
+    c.execute(query)
+    # Fetch the results of the query then close the db connection
+    articles = c.fetchall()
+    db.close()
+    # Print the article title and no. of hits
+    for article in articles:
+        title_format = article[0]
+        title = title_format.title()
+        hits = article[1]
+        print("{0} – {1} views".format(title, hits))
 
-def get_top_authors():
-  """Returns which authors get the most page views as a sorted list with the most popular author at the top."""
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  # Execute a query for the top three articles using 'popular' view of log table
-  query = ("select name, sum(hits) as views from bylines, popular where bylines.slug = popular.substring news-> group by name order by views desc")
-  c.execute(query)
-  # Fetch the results of the query then close the db connection
-  authors = c.fetchall()
-  db.close()
-  # Print the article title and no. of hits
-  for author in authors:
-      author = author[0]
-      views = author[1]
-      print ('{} – {} views'.format(author, views))
+    def get_top_authors():
+        ''''Prints which authors get the most page views'''
+
+        db = psycopg2.connect("dbname=news")
+        c = db.cursor()
+        query = ("select * from authorviews limit 5")
+        c.execute(query)
+        authors = c.fetchall()
+        db.close()
+        # author = authors[0]
+        for author in authors:
+            print("{} – {} views".format(author[0], author[1]))
+
+    def get_errors():
+        '''Prints days when more than 1% of requests led to errors'''
+
+        db = psycopg2.connect("dbname=news")
+        c = db.cursor()
+        query = ("select to_char(day, 'Month DD, YYYY') as date, percent from final where percent > 1;")
+        c.execute(query)
+        errors = c.fetchall()
+        db.close()
+        for error in errors:
+            print("{} – {}% errors".format(error[0], error[1]))
+
+    print("What are the most popular three articles of all time? \n")
+    get_top_three_articles()
+
+    print("\nWho are the most popular article authors of all time?\n")
+    get_top_authors()
+
+    print("\nOn which days did more than 1% of requests lead to errors?")
+    get_errors()
